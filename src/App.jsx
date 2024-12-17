d3a175ca316852f2e7dc8ceb60a908208acc3126
Usman Jamil <ujamil@ad.mcmso.co>
2024-12-17 20:06:26 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>React Redux</h1>
+    <AddTodo />
+    <Todos />
+    
+    </>
+  )
+}
+
+export default App
